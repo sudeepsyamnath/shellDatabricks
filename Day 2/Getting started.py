479ce71f8a2a125658ecbe277a34f543d300768b
sudeepsays@gmail.com <sudeepsays@gmail.com>
2024-01-09 09:23:08 +0000

    Day 2 file and folder added

---
+++ Day 2/Getting started.py
@@ -0,0 +1,41 @@
+# Databricks notebook source
+print("Run Python")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Getting started with spark
+# MAGIC ## Databricks
+# MAGIC ### Delta lake
+# MAGIC ###### Magic command
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC println("RUn scala")
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- select "RUN SQL"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Header
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Databricks Repos
+# MAGIC Step1: Add repo
+# MAGIC Step2: set up integration
+
+# COMMAND ----------
+
+
